2376bf0018740dfca49d6c371a0ea64535daeb8a
HajinCity <128238629+HajinCity@users.noreply.github.com>
2024-08-31 16:30:30 +0800

    Carlos Commit

---
+++ app/src/main/java/com/example/tuluyanapp/classes/Landlord.java
@@ -0,0 +1,30 @@
+package com.example.tuluyanapp.classes;
+
+public class Landlord {
+    // Attributes
+    private int landlordID;
+    private String landlordFirstName;
+    private String landlordMiddleName;
+    private String landlordLastname;
+    private String landlordAddress;
+
+    // Constructor
+    public Landlord(int landlordID, String landlordFirstName, String landlordMiddleName, String landlordLastname, String landlordAddress) {
+        // Initialize attributes
+    }
+
+    // Method to add a room
+    public void addRoom(BoardingHouseRoom room) {
+        // Logic to add room
+    }
+
+    // Method to remove a room
+    public void removeRoom(BoardingHouseRoom room) {
+        // Logic to remove room
+    }
+
+    // Method to view tenant applications
+    public void viewTenantApplications() {
+        // Logic to view tenant applications
+    }
+}
