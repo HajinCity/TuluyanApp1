4e96a1c52207d3a938974fb505a6ea6f3a7b805d
HajinCity <128238629+HajinCity@users.noreply.github.com>
2024-09-11 01:13:18 +0800

    Carlos Commit 09/11/2024 01:13AM

---
+++ app/src/main/java/com/example/tuluyanapp/ownerfgp.java
@@ -0,0 +1,25 @@
+package com.example.tuluyanapp;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ownerfgp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.owner_forgot_password);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
